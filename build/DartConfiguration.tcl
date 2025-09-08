1cf4720d3d240a12cfa7816203051359b1a3117e
Simone Laudadio <simonelaudadio@studio.unibo.it>
2025-09-08 23:22:10 +0200

    Modifiche build

--- build/DartConfiguration.tcl
+++ build/DartConfiguration.tcl
@@ -4,14 +4,14 @@
 
 
 # Configuration directories and files
-SourceDirectory: /home/gg/Downloads/progetto
-BuildDirectory: /home/gg/Downloads/progetto/build
+SourceDirectory: /home/simo-06/Lotka-Volterra
+BuildDirectory: /home/simo-06/Lotka-Volterra/build
 
 # Where to place the cost data store
 CostDataFile: 
 
 # Site is something like machine.domain, i.e. pragmatic.crd
-Site: gg-Legion-Go-8APU1
+Site: Simone
 
 # Build name is osname-revision-compiler, i.e. Linux-2.4.2-2smp-c++
 BuildName: Linux-c++
@@ -27,7 +27,7 @@ SubmitInactivityTimeout:
 NightlyStartTime: 00:00:00 EDT
 
 # Commands for the build/test/submit cycle
-ConfigureCommand: "/usr/bin/cmake" "/home/gg/Downloads/progetto"
+ConfigureCommand: "/usr/bin/cmake" "/home/simo-06/Lotka-Volterra"
 MakeCommand: /usr/bin/cmake --build . --config "${CTEST_CONFIGURATION_TYPE}"
 DefaultCTestConfigurationType: Release
 
